d285e5f9a7c34ac7410c7941bf7cb0b8e5919956
JSL107 <95362504+JSL107@users.noreply.github.com>
2022-03-23 18:00:06 +0900

    JPA

--- 220322/bookorder/src/main/java/dev/book/bookorder/repository/OrderReposiory.java
+++
@@ -1,9 +0,0 @@
-package dev.book.bookorder.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.book.bookorder.model.entity.Order;
-
-public interface OrderReposiory extends JpaRepository<Order, Long> {
-
-}
