848eb0911b0962879d7d7eae079844608c2c0573
KimEunhyuk <bang1676@naver.com>
2022-04-11 18:52:01 +0900

    Delete 220323 directory

--- 220323/src/main/java/dev/book/bookorder/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package dev.book.bookorder.service;
-
-import java.util.List;
-
-import dev.book.bookorder.model.dto.BookDTO;
-
-public interface BookService { // class BookServiceImpl
-    List<BookDTO> findAllBooks();
-}
